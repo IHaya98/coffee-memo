0df9598ea6bc963ed9b071abfeeccd2ebeccca0e
IHaya98 <59387614+IHaya98@users.noreply.github.com>
2025-09-27 12:30:03 +0000

    修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import Navigation from '../components/Navigation'
 import { headers } from 'next/headers'
 import React from 'react'
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
